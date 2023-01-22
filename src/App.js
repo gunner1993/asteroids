34638dc3623dcad962be62bc7ac4d73c52abb99d
Josip <papezjosip@gmail.com>
2023-01-22 17:04:08 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import { useRecoilState } from "recoil";
+import { Home } from "./components/Home";
+import NavBar from "./components/NavBar";
+import { cardsData } from "./store/recoilStore";
 
-function App() {
+const App = () => {
+  const [cards, setCards] = useRecoilState(cardsData);
+
+  useEffect(() => {
+    fetch(
+      "https://api.nasa.gov/neo/rest/v1/feed?start_date=2021-12-16&api_key=oIrmVgsSPsjCau12VhdHTVEEY6vK5uZPujX9KFSD"
+    )
+      .then(response => response.json())
+      .then(json => setCards(json));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/biggest" element={<Home />} />
+        <Route path="/hazardous" element={<Home />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/ImgList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ImgList = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ImgList
